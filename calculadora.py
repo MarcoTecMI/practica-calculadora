e967732e0c1fc7d5bc1def3a2b0fc1f3febf82d8
Marco <al07100539@tecmilenio.mx>
2026-03-23 12:23:17 -0700

    Agregamos la funcion de suma

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,10 @@
 def main():
-    pass
+    n1 = int(input("Dame el primer numero: "))
+    n2 = int(input("Dame el segundo numero: "))
+
+    suma = n1 + n2
+    print(f"La respuesta de la suma es {suma}")
+    
 
 if __name__ == "__main__":
     main()
