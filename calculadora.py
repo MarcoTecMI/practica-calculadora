f96266de78300f46f7972b296f50bd1eb4293c25
Gael <gaelfortnitegpm@gmail.com>
2026-03-23 13:38:18 -0700

    gael agrego la resta

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,8 @@
-def main():
-    pass
+def resta():
+    n1 = 10
+    n2 = 5
+    resta = n1 - n2
+    print(f"el resultado de la resta es: {resta}")
 
 if __name__ == "__main__":
-    main()
+    resta()
