69940137aafbd1cbaa35b956462c3b6b5f95a0c6
EdgarAlvarado31 <al07035361@tecmilenio.mx>
2026-03-23 20:08:19 -0700

    Agregue funcion de multiplicacion

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,14 @@
+def multiplicacion(a, b):
+    return a * b
+
 def main():
-    pass
+    print("Función de multiplicación")
+
+    num1 = float(input("Ingresa el primer número: "))
+    num2 = float(input("Ingresa el segundo número: "))
+
+    resultado = multiplicacion(num1, num2)
+    print("Resultado:", resultado)
 
 if __name__ == "__main__":
     main()
