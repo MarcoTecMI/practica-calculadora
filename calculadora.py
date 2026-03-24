23a59bfd778223ce725391e831c9870e3df55e5d
EdgarAlvarado31 <al07035361@tecmilenio.mx>
2026-03-23 18:48:37 -0700

    Agregado de funcion division

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,21 @@
+def division(a, b):
+    if b == 0:
+        print("Error: no se puede dividir entre 0")
+        return None
+    return a / b
+
+
 def main():
-    pass
+    print("Función de división")
+
+    num1 = float(input("Ingresa el primer número: "))
+    num2 = float(input("Ingresa el segundo número: "))
+
+    resultado = division(num1, num2)
+
+    if resultado != None:
+        print("Resultado:", resultado)
+
 
 if __name__ == "__main__":
     main()
